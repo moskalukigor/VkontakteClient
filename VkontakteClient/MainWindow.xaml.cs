e675450b4bd2facd81289c2dc10e020b035dc028
moskalukigor <moskalukigor@mail.ru>
2015-02-22 12:02:27 +0200

    AddMenu
    Audio(100%) - working!
    Friends(10%) - not working!

--- VkontakteClient/MainWindow.xaml.cs
+++ VkontakteClient/MainWindow.xaml.cs
@@ -32,181 +32,68 @@ namespace VkontakteClient
 
 
         
-        public List<Audio> audioList;
-        private readonly BackgroundWorker worker = new BackgroundWorker();
         
+        //public List<Audio> audioList;
+        private readonly BackgroundWorker worker = new BackgroundWorker();
+
         public MainWindow()
         {
             InitializeComponent();
             new AuthorizationForm().Show();
 
-            worker.DoWork += worker_DoWork;
-            worker.RunWorkerAsync();
-
-            btnStart.IsEnabled = false;
-            btnStop.IsEnabled = false;
-            btnPause.IsEnabled = false;
-        }
-
-        public class Audio
-        {
-            public int id { get; set; }
-            public int owner_id { get; set; }
-            public string artist { get; set; }
-            public string title { get; set; }
-            public int duration { get; set; }
-            public string url { get; set; }
-            public string lurics_id { get; set; }
-            public int genre_id { get; set; }
-        }
-        private void worker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            while (!Settings1.Default.auth)
-            {
-                Thread.Sleep(500);
-            }
-            WebRequest request =
-                WebRequest.Create(String.Format("https://api.vk.com/method/audio.get?owner_id={0}&access_token={1}", Settings1.Default.id, Settings1.Default.token));
-            WebResponse response = request.GetResponse();
-            Stream dataStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
-            string responseFromServer = reader.ReadToEnd();
-            reader.Close();
-            response.Close();
-            responseFromServer = HttpUtility.HtmlDecode(responseFromServer);
-
-            JToken token = JToken.Parse(responseFromServer);
-            audioList = token["response"].Children().Skip(1).Select(c => c.ToObject<Audio>())
-                .ToList();
-
-            Dispatcher.Invoke((Action)delegate 
-            { 
-                for(int i = 0; i < audioList.Count(); i++)
-                {
-                    lbxAudio.Items.Add(audioList[i].artist + " - " + audioList[i].title);
-                }
-            });
-        }
-
-        private void lbxAudio_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            if(e.ChangedButton == MouseButton.Left)
-            {
-                mediaAudio.Source = new Uri(audioList[lbxAudio.SelectedIndex].url);
-                mediaAudio.Pause();
-                DispatcherTimer dispatcherTimer = new DispatcherTimer();
-                dispatcherTimer.Tick += new EventHandler(timer_Tick);
-                dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
-                dispatcherTimer.Start();
-                btnStart.IsEnabled = true;
-                btnStop.IsEnabled = false;
-                btnPause.IsEnabled = false;
-                sliderDuration.Value = 0;
-            }
-        }
-        void timer_Tick(object sender, EventArgs e)
-        {
-            if(btnStart.Content == "Play" && btnStart.IsEnabled == false)
-            sliderDuration.Value = mediaAudio.Position.TotalSeconds;
-        }
-
-        private void btnStart_Click(object sender, RoutedEventArgs e)
-        {
-              mediaAudio.Play();
-              btnStart.IsEnabled = false;
-              btnPause.IsEnabled = true;
-              btnStop.IsEnabled = true;
-              btnStart.Content = "Play";
-        }
-
-        private void btnStop_Click(object sender, RoutedEventArgs e)
-        {
-            mediaAudio.Stop();
-            btnStart.IsEnabled = true;
-            btnStop.IsEnabled = false;
-            btnPause.IsEnabled = false;
-            btnStart.Content = "Play";
-            sliderDuration.Value = 0;
-        }
-
-        private void btnPause_Click(object sender, RoutedEventArgs e)
-        {
-            mediaAudio.Pause();
-            btnStart.IsEnabled = true;
-            btnPause.IsEnabled = false;
-            btnStart.Content = "Resume";
-        }
-
-        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            TimeSpan ts = TimeSpan.FromSeconds(e.NewValue);
-            mediaAudio.Position = ts;
-        }
-
-        private void mediaAudio_MediaOpened(object sender, RoutedEventArgs e)
-        {
-            if(mediaAudio.NaturalDuration.HasTimeSpan)
-            {
-                TimeSpan ts = TimeSpan.FromMilliseconds(mediaAudio.NaturalDuration.TimeSpan.TotalMilliseconds);
-                sliderDuration.Maximum = ts.TotalSeconds;
-            }
         }
 
 
-
-
-        
-
-
-
-        /*private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        public void InvisibleAllUC()
         {
-            //update ui once worker complete his work
-        }*/
-        /*private void webBrowser1_LoadCompleted(object sender, NavigationEventArgs e)
+            UCAudio.Visibility = Visibility.Hidden;
+            UCFriends.Visibility = Visibility.Hidden;
+        }
+        private void btnMusic_Click(object sender, RoutedEventArgs e)
         {
-            string url = webBrowser1.Source.ToString();
-            if(Settings1.Default.UserId == null)
-            { 
-                if(url.IndexOf("access_token") != -1 )
-                {
-                    Regex token_patt = new Regex("access_token=(.+)&expires_in");
-                    Match token_match = token_patt.Match(url);
-                    Regex user_patt = new Regex("user_id=(.+)&email");
-                    Match user_match = user_patt.Match(url);
+            InvisibleAllUC();
+            UCAudio.Visibility = Visibility.Visible;
+        }
+        private void btnFriends_Click(object sender, RoutedEventArgs e)
+        {
+            InvisibleAllUC();
+            UCFriends.Visibility = Visibility.Visible;
+        }
 
-                    if((!token_match.Success) || (!user_match.Success)) // якщо токен не знайдено
-                    {
-                        MessageBox.Show("Ошибка");
-                        MessageBox.Show(url);
-                        Application.Current.Shutdown();
-                    }
-
-                    else
-                    {
-                        Settings1.Default.AccessToken = token_match.Groups[1].Value.ToString();
-                        Settings1.Default.UserId = user_match.Groups[1].Value.ToString();
-
-                        webBrowser1.Visibility = Visibility.Hidden;
-                    }
-                }
-            }
-            else
-            {
-                MessageBox.Show(e.Uri.AbsolutePath);
-
-                var xml = new XmlDocument();
-                xml.Load(url);
-
-                xml.Save("XMLAudioCount");
-            }
-        }*/
-        /*private void btnGetCountAudio_Click(object sender, RoutedEventArgs e)
+        private void btnNews_Click(object sender, RoutedEventArgs e)
         {
 
-            webBrowser1.Navigate(string.Format("https://api.vk.com/method/audio.getCount.xml?oid={0}&access_token={1}",
-                Settings1.Default.UserId, Settings1.Default.AccessToken));
-        }*/
+        }
+
+        private void btnFeedback_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMessages_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCommunities_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPhoto_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnVideos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSettings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
 
     }
 }
---
+++ VkontakteClient/UserControls/UCAudio.xaml.cs
@@ -0,0 +1,160 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace VkontakteClient.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl1.xaml
+    /// </summary>
+    public partial class UCAudio : UserControl
+    {
+
+        public List<Audio> audioList;
+        private readonly BackgroundWorker worker = new BackgroundWorker();
+
+        public UCAudio()
+        {
+            InitializeComponent();
+
+            worker.DoWork += worker_DoWork;
+            worker.RunWorkerAsync();
+
+            btnStart.IsEnabled = false;
+            btnStop.IsEnabled = false;
+            btnPause.IsEnabled = false;
+        }
+
+        public class Audio
+        {
+            public int id { get; set; }
+            public int owner_id { get; set; }
+            public string artist { get; set; }
+            public string title { get; set; }
+            public int duration { get; set; }
+            public string url { get; set; }
+            public string lurics_id { get; set; }
+            public int genre_id { get; set; }
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (!Settings1.Default.auth)
+            {
+                Thread.Sleep(500);
+            }
+            WebRequest request =
+                WebRequest.Create(String.Format("https://api.vk.com/method/audio.get?owner_id={0}&access_token={1}", Settings1.Default.id, Settings1.Default.token));
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            response.Close();
+            responseFromServer = HttpUtility.HtmlDecode(responseFromServer);
+
+            JToken token = JToken.Parse(responseFromServer);
+            audioList = token["response"].Children().Skip(1).Select(c => c.ToObject<Audio>())
+                .ToList();
+
+            Dispatcher.Invoke((Action)delegate
+            {
+                for (int i = 0; i < audioList.Count(); i++)
+                {
+                    lbxAudio.Items.Add(audioList[i].artist + " - " + audioList[i].title);
+                }
+            });
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (btnStart.Content == "Play" && btnStart.IsEnabled == false)
+                sliderDuration.Value = mediaAudio.Position.TotalSeconds;
+            //lblDuration.Content = (int)mediaAudio.Position.TotalSeconds + "/" + mediaAudio.NaturalDuration.TimeSpan.TotalMinutes;
+        }
+
+        private void mediaAudio_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            if (mediaAudio.NaturalDuration.HasTimeSpan)
+            {
+                TimeSpan ts = TimeSpan.FromMilliseconds(mediaAudio.NaturalDuration.TimeSpan.TotalMilliseconds);
+                sliderDuration.Maximum = ts.TotalSeconds;
+            }
+        }
+
+        private void lbxAudio_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                mediaAudio.Source = new Uri(audioList[lbxAudio.SelectedIndex].url);
+                mediaAudio.Pause();
+                DispatcherTimer dispatcherTimer = new DispatcherTimer();
+                dispatcherTimer.Tick += new EventHandler(timer_Tick);
+                dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+                dispatcherTimer.Start();
+                btnStart.IsEnabled = true;
+                btnStart.Content = "Play";
+                btnStop.IsEnabled = false;
+                btnPause.IsEnabled = false;
+                sliderDuration.Value = 0;
+
+            }
+        }
+
+        private void sliderDuration_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TimeSpan ts = TimeSpan.FromSeconds(e.NewValue);
+            mediaAudio.Position = ts;
+        }
+
+        private void sliderVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            mediaAudio.Volume = e.NewValue;
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            mediaAudio.Play();
+            btnStart.IsEnabled = false;
+            btnPause.IsEnabled = true;
+            btnStop.IsEnabled = true;
+            btnStart.Content = "Play";
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            mediaAudio.Stop();
+            btnStart.IsEnabled = true;
+            btnStop.IsEnabled = false;
+            btnPause.IsEnabled = false;
+            btnStart.Content = "Play";
+            sliderDuration.Value = 0;
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            mediaAudio.Pause();
+            btnStart.IsEnabled = true;
+            btnPause.IsEnabled = false;
+            btnStart.Content = "Resume";
+        }
+    }
+}
